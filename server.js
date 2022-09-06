9086d82f7c88fdac88b3886f968d9f759e28be31
EduardoSilva09 <silvafeitosaeduardo78@gmail.com>
2022-09-06 11:08:35 -0300

    Adicionada tratativa para erros no servidor

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ const route = router.get('/', (req, res, next) => {
 
 app.use('/', route)
 server.listen(port)
+server.on('error', onError)
+
 console.log(`Server is running... port : ${port} `);
 
 function normalizePort(val) {
@@ -36,4 +38,27 @@ function normalizePort(val) {
     }
 
     return false
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port
+
+    switch (error.code) {
+        case 'EACCES':
+            console.log(bind + ' requires elevated privileges');
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            console.log(bind + ' is already in use');
+            process.exit(1)
+            break
+        default:
+            throw error
+    }
 }
