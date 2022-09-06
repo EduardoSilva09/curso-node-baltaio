5228b24482d4f59f733f97f07e438203669aa7ef
EduardoSilva09 <silvafeitosaeduardo78@gmail.com>
2022-09-06 15:23:08 -0300

    Configuração da  instância do mongo DB

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 const router = express.Router()
+
+mongoose.connect('')
+
 const indexRoutes = require('./routes/index-routes')
 const productsRoutes = require('./routes/product-routes')
 
