57354e7ef22b9235399a24e798aacc1377a768a3
EduardoSilva09 <silvafeitosaeduardo78@gmail.com>
2022-09-06 16:05:39 -0300

    Criada rota para listar os produtos ativos cadastrados

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,18 @@
 const mongoose = require("mongoose")
 const Product = mongoose.model('Product')
 
+exports.get = (req, res, next) => {
+    Product.find({
+        active: true
+    }, 'title price slug')
+    .then(data => {
+        res.status(200).send(data)
+    })
+    .catch(e => {
+        res.status(400).send({ message: 'Falha ao listar produtos.', data: e })
+    })
+}
+
 exports.post = (req, res, next) => {
     const product = new Product(req.body)
     product.save()
