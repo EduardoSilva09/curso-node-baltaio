6723e80d4da685622e56ca29b6c78856df017fa2
EduardoSilva09 <silvafeitosaeduardo78@gmail.com>
2022-09-09 07:45:13 -0300

    Implementada função para encriptar a senha do customer antes de salvar

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require('../validators/validator')
 const repository = require('../repositories/customer-repository.js')
+const md5 = require('md5')
 
 exports.get = async (req, res, next) => {
     try {
@@ -27,7 +28,13 @@ exports.post = async (req, res, next) => {
     }
 
     try {
-        await repository.create(req.body)
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            password: md5(req.body.password + global.SALT_KEY)
+        }
+
+        await repository.create(data)
         res.status(201).send({ message: 'Cliente cadastrado com sucesso!' })
     }
     catch (e) {
