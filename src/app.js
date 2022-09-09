958047d14794bee64ad65ee77f67b09e04ac6e67
EduardoSilva09 <silvafeitosaeduardo78@gmail.com>
2022-09-09 10:52:00 -0300

    Liberado CORS da API

--- src/app.js
+++ src/app.js
@@ -30,6 +30,14 @@ app.use(bodyParser.json({
 })) // todo o conteúdo será convertido para json
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//Habilita o CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next()
+})
+
 app.use('/', indexRoutes)
 app.use('/customers', customersRoutes)
 app.use('/orders', ordersRoutes)
